6d8f03713852441bed36d646eb0e49f4ed62c6ee
Sofía Gonzalez <sofiiaglz@hotmail.com>
2024-03-25 10:27:57 -0600

    Initial Commit

---
+++ Ahorcado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ahorcado
+//
+//  Created by Mac19 on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
