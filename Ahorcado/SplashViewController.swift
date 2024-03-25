bdd464c7332ec39a096445f7aec7fc98d6214772
Sofía Gonzalez <sofiiaglz@hotmail.com>
2024-03-25 13:47:53 -0600

    splash funciona

--- Ahorcado/SplashViewController.swift
+++ Ahorcado/SplashViewController.swift
@@ -13,20 +13,15 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let w = imgSplash.frame.width/2
-        let h = imgSplash.frame.height/2
-        let x = (view.frame.width - w)/2
-        let y = -h
-        imgSplash.frame = CGRect(x: x, y: y, width: w, height: h)
-//        imgSplash.frame.origin.x = (view.frame.width - imgSplash.frame.width)/2.0
-//        imgSplash.frame.origin.y = view.frame.height
+       
+
     }
     override func viewDidAppear(_ animated: Bool) {
         UIView.animate(withDuration: 2){
             self.imgSplash.frame.origin.y = (self.view.frame.height - self.imgSplash.frame.height)/2.0
             self.imgSplash.alpha = 1.0
         } completion: { respuesta in
-            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
+            Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false) { timer in
                 self.performSegue(withIdentifier: "sgSplash", sender: nil)
             }
         }
